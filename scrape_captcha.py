d731fa3809f9fead12a36a40e6e720866b5d99cf
Alexander Cohen <alex@capitolmuckraker.com>
2020-08-24 19:43:49 -0400

    initial commit

---
+++ scrape_captcha.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+# scrape Arlington, VA court cases using Selenium and Firefox on Mac OSX
+from __future__ import print_function
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import StaleElementReferenceException,NoSuchElementException,TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
+from time import sleep
+import os, logging
+
+def driver():
+    fp = webdriver.FirefoxProfile()
+    driver = webdriver.Firefox(firefox_profile=fp)
+    return driver
+
+def captcha_start_page(driver=driver):
+    start_url = 'https://eapps.courts.state.va.us/gdcourts/captchaVerification.do?landing=landing'
+    driver.get(start_url)
+    element = driver.find_element_by_name("accept")
+    element.click()
+
+def captcha_start_arlington_court(driver=driver):
+    element = driver.find_element_by_id("txtcourts1")
+    courtname = "Arlington General District Court"
+    element.send_keys(courtname)
+    element.click()
+    driver.implicitly_wait(2)
+    element.click()
+
+def captcha_start_date_search(driver=driver,date = "08/03/2020"):
+    element = driver.find_element_by_link_text("Hearing Date Search")
+    element.click()
+    driver.implicitly_wait(10)
+    element = driver.find_element_by_id("txthearingdate")
+    element.send_keys(date)
+    element.send_keys(Keys.ENTER)
+
+def captcha_start_scrape_results_page(driver=driver):
+    results = driver.find_element_by_tag_name("table").text[1036:][:-178]
+    return results
+
+def captcha_start_results_next_page(driver=driver):
+    try:
+        element = driver.find_element_by_name("caseInfoScrollForward")
+        element.click()
+        return True
+    except NoSuchElementException:
+        print("End of results")
+        return False
+
---
+++ scrape_no_captcha.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+# scrape Arlington, VA court cases using Selenium and Firefox on Mac OSX
+from scrape_captcha import *
+from time import sleep
+
+driver = driver()
+
+driver.get("https://eapps.courts.state.va.us/ocis/landing/false")
+sleep(1)
+
+element = driver.find_element_by_id("acceptTerms")
+element.click()
+sleep(0.5)
+
+driver.find_element_by_id("searchBy").click()
+sleep(2)
+
+element = driver.find_element_by_css_selector("input[type='radio'][value='Hearing Date']").click()
+sleep(1)
+
+driver.find_element_by_id("searchBy").click()
+sleep(0.6)
+
+driver.find_element_by_id("courtLevelSelectBtn").click()
+sleep(0.4)
+
+driver.find_element_by_id("courtLevelLabel0").click()
+sleep(1)
+
+driver.find_element_by_id("courtLevelSelectBtn").click()
+sleep(1)
+
+driver.find_element_by_id("allCourtTitle").click()
+sleep(0.5)
+
+element = driver.find_element_by_id("courtSearch")
+element.send_keys("Arlington")
+sleep(0.4)
+
+driver.find_element_by_id("selectCourtSpan0").click()
+sleep(0.6)
+
+driver.find_element_by_id("applyCourtBtn").click()
+sleep(1)
+
+element = driver.find_element_by_id("datepickerele")
+date = "08/03/2020"
+element.send_keys(date)
+element.send_keys(Keys.ENTER)
+sleep(1)
+
+element = driver.find_element_by_class_name("search-results")
+print(element.text[76:])
