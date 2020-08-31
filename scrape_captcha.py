3d620f2aa00e0fde5b9f68091fe5f2adbcffc532
Alexander Cohen <alex@capitolmuckraker.com>
2020-08-31 12:17:18 -0400

    import ElementClickInterceptedException

--- scrape_captcha.py
+++ scrape_captcha.py
@@ -3,7 +3,7 @@
 from __future__ import print_function
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import StaleElementReferenceException,NoSuchElementException,TimeoutException
+from selenium.common.exceptions import StaleElementReferenceException,NoSuchElementException,TimeoutException,ElementClickInterceptedException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
