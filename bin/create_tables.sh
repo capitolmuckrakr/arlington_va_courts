e65ba0ac3e84c740ce0077f74f4f7c1b90ad6d03
Alexander Cohen <alex@capitolmuckraker.com>
2020-09-01 14:10:20 -0400

    set var name for db endpoint

--- bin/create_tables.sh
+++ bin/create_tables.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export SQL_DIR="${HOME}/scripts/arlington_va_courts/bin/sql/"
-psql --host=${ENDPOINT} --port=5432 --username=alex --dbname=arlingtoncourts -f ${SQL_DIR}cases_summary.sql
+psql --host=${ENDPOINT_DB} --port=5432 --username=alex --dbname=arlingtoncourts -f ${SQL_DIR}cases_summary.sql
